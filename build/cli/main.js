f150500678a26109cb45f520c231226fcf8f23ff
justinwoo <moomoowoo@gmail.com>
2017-09-30 22:27:41 +0300

    push gitignored files for demo

---
+++ build/cli/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var OutputPeople = require("../output/People");
+var People = OutputPeople;
+console.log(People.david);
---
+++ build/output/People/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var david = {
+    name: "David "
+};
+module.exports = {
+    david: david
+};
